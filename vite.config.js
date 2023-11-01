4c83277fdeae2ec1d100928592017b99bdef7eed
darazuiscaptain <smallcaptain@proton.me>
2023-11-01 17:03:42 +0300

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+
+// vite.config.js
+module.exports = {
+  build: {
+    outDir: './dist',
+  },
+};
