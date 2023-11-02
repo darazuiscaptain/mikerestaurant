b65a63e7a88547c7801fc26be5b7453519a7c7b9
darazuiscaptain <smallcaptain@proton.me>
2023-11-02 18:34:06 +0300

    About and caontact UI

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='flex flex-col gap-5 max-w-[500px] mx-auto'>
-      <p className='font-serif'>
+    <div className='flex flex-col gap-5 max-w-[500px] mx-auto px-3 font-serif'>
+      <h1 className='text_gradient_h text-2xl uppercase font-semibold'>Lorem ipsum dolor</h1>
+      <p className=''>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit.
         Odio similique voluptatum magnam earum fuga libero tempora sint nobis ipsa voluptates.
-        Culpa error earum laborum accusantium?
+        Culpa error earum laborum accusantium.
       </p>
 
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Ut, distinctio placeat ea neque qui corrupti sunt rem sed culpa maxime magni, 
         quae repudiandae blanditiis similique ipsam dolor ipsum exercitationem optio! 
-        Aliquam similique quis voluptates eaque!
+        Aliquam similique quis voluptates eaque.
       </p>
 
       <p>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,24 @@
+import { Card } from '@material-tailwind/react'
 import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <section className='flex flex-col justify-center gap-5 w-full max-w-[500px] mx-auto'>
+      <Card className='flex flex-col justify-between px-5 sm:px-12'>
+        <h1 className='text_gradient_p text-2xl text-center underline-offset-2'>Contact Us</h1>
+        <form className='flex flex-col w-full h-full gap-3 my-4'>
+          <input type="text" placeholder='Full name' className='hover:outline-none placeholder:text-blue-gray-100 p-3 shadow-md rounded-lg' />
+          <input type="text" placeholder='Email' className='hover:outline-none placeholder:text-blue-gray-100 p-3 shadow-md rounded-lg'/>
+          <input type="text" placeholder='Subject' className='hover:outline-none placeholder:text-blue-gray-100 p-3 shadow-md rounded-lg'/>
+          <textarea name="body" className='max-h-24 hover:outline-none placeholder:text-blue-gray-100 p-3 shadow-md rounded-lg' cols="10" rows="10" placeholder='Write here..'>
+          </textarea>
+          <button className='w-full uppercase p-3 rounded-lg my-9 bg-green-900 text-white opacity-90'>
+            send
+          </button>
+        </form>
+      </Card>
+
+    </section>
   )
 }
 
