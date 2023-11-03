4af0ce5d078e7b8a17053af58f2649ff1860c12c
darazuiscaptain <smallcaptain@proton.me>
2023-11-03 22:34:56 +0300

    contact page functionality

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,24 +1,61 @@
 import { Card } from '@material-tailwind/react'
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Contact() {
+  const emailTo = "smikiyas37@gmail.com"
+  const [ emailFrom, sendEmailFrom ] = useState({
+    full_name: "",
+    email: "",
+    subject: "",
+    message: ""
+  })
+  const handleChange = (e) => {
+    sendEmailFrom({...emailFrom, [e.target.name]: e.target.value})
+  }
+
+  const { full_name, email, subject, message } = emailFrom
   return (
     <section className='flex flex-col justify-center gap-5 w-full max-w-[500px] mx-auto'>
-      <Card className='flex flex-col justify-between px-5 sm:px-12'>
-        <h1 className='text_gradient_p text-2xl text-center underline-offset-2'>Contact Us</h1>
+      <Card className='flex flex-col justify-between px-5 sm:px-12 drop-shadow-2xl'>
+        <h1 className='text_gradient_p text-2xl text-center underline-offset-2 pt-2'>Contact Us</h1>
         <form className='flex flex-col w-full h-full gap-3 my-4'>
-          <input type="text" placeholder='Full name' className='hover:outline-none placeholder:text-blue-gray-100 p-3 shadow-md rounded-lg' />
-          <input type="text" placeholder='Email' className='hover:outline-none placeholder:text-blue-gray-100 p-3 shadow-md rounded-lg'/>
-          <input type="text" placeholder='Subject' className='hover:outline-none placeholder:text-blue-gray-100 p-3 shadow-md rounded-lg'/>
-          <textarea name="body" className='max-h-24 hover:outline-none placeholder:text-blue-gray-100 p-3 shadow-md rounded-lg' cols="10" rows="10" placeholder='Write here..'>
+          <input 
+            type="text" 
+            name="full_name"
+            value={full_name}
+            onChange={handleChange}
+            placeholder='Full name' 
+            className='hover:outline-none placeholder:text-blue-gray-100 p-2 shadow-lg rounded-lg' />
+          <input 
+            type="text" 
+            name="email"
+            value={email}
+            onChange={handleChange}
+            placeholder='Email' 
+            className='hover:outline-none placeholder:text-blue-gray-100 p-2 shadow-lg rounded-lg'/>
+          <input 
+            type="text" 
+            name="subject"
+            value={subject}
+            onChange={handleChange}
+            placeholder='Subject' 
+            className='hover:outline-none placeholder:text-blue-gray-100 p-2 shadow-lg rounded-lg'/>
+          <textarea 
+            name="message" 
+            value={message}
+            onChange={handleChange}
+            className='max-h-24 hover:outline-none placeholder:text-blue-gray-100 p-2 shadow-lg rounded-lg' 
+            cols="10" rows="10" placeholder='Write here..'>
           </textarea>
-          <button className='w-full uppercase p-3 rounded-lg my-9 bg-gradient-to-r from-[#c0146a] to-[#36cd08] text-white opacity-90'>
+          <Link 
+            to={`mailto:${emailTo}?subject=Regarding ${subject}&body=HeyI'm ${full_name},  ${message}`}
+            className='text-center w-full uppercase p-3 rounded-lg my-3 bg-[#1d5e22] text-white opacity-90'>
             send
-          </button>
+          </Link>
         </form>
       </Card>
     </section>
-  )
+  ) 
 }
-
 export default Contact
