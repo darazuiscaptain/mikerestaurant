d7c224126fc150f94e1b56e6735ae9a7818cb1c8
darazuiscaptain <smallcaptain@proton.me>
2023-11-05 21:43:10 +0300

    api for update profile

--- server/controller/users.controller.js
+++ server/controller/users.controller.js
@@ -1,7 +1,40 @@
 import User from "../model/users.model.js"
+import bcrypt from "bcrypt"
 
 export const getUsers = async (req, res, next) => {
     const users = await User.find()
     res.json(users)
 }
 
+export const getOneUser = async (req, res, next) => {
+    const { id } = req.params
+
+    try {
+        const user = await User.findById({ _id: id })
+        if (user) return res.status(200).json(user)
+        return res.status(204).json("No Content")
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateUser = async (req, res, next) => {
+    const { id } = req.params
+    const { username, password, photo } = req.body
+    try {
+        const user = await User.findById({ _id: id })
+        if (!user) return res.status(400).json("Invalid update")
+
+        const hashedPassword = bcrypt.hashSync(password, 10)
+        const updatedUser = await User.findByIdAndUpdate({ _id: id }, {
+            username: username,
+            password: hashedPassword,
+            photo: photo 
+        }, { new: true })
+
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        next(error)
+    }
+}
--- server/route/users.router.js
+++ server/route/users.router.js
@@ -1,8 +1,10 @@
 import express from "express"
-import {  getUsers } from "../controller/users.controller.js"
+import {  getUsers, getOneUser, updateUser } from "../controller/users.controller.js"
 
 const router = express.Router()
 
 router.get("/", getUsers)
+router.get("/:id", getOneUser)
+router.put("/:id", updateUser)
 
 export default router;
