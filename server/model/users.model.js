fce3131680f67bbd1d4f4bc2028302c000448096
darazuiscaptain <smallcaptain@proton.me>
2023-11-05 12:49:14 +0300

    email validation

--- server/model/users.model.js
+++ server/model/users.model.js
@@ -1,17 +1,25 @@
 import mongoose from "mongoose";
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         unique: true,
-        trim: true
+        trim: true,
+        lowercase: true
     },
     email: {
         type: String,
-        required: true,
+        trim: true,
+        lowercase: true,
         unique: true,
-        trim: true 
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'] 
     },
     password: {
         type: String,
