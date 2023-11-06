9674981aec512c090c23649712406aba9968fd0c
darazuiscaptain <smallcaptain@proton.me>
2023-11-06 19:50:27 +0300

    updates

--- client/src/UI/Cart.jsx
+++ client/src/UI/Cart.jsx
@@ -5,11 +5,11 @@ function Cart() {
         <csection id="cart" className="p-2">
             <div className='flex flex-col gap-5'>
                 <h1 className='text-2xl font-semibold text-gray-700 text-center underline'>
-                    Your Basket
+                    Your Cart
                 </h1>
                 <div className='flex flex-col mx-2'>
                     <div className='flex flex-1 gap-2 h-96'>
-                        <h1>name</h1>    
+                        <h1>Cart List</h1>    
 
                     </div>
                     <button className='flex w-full text-center justify-center p-1 rounded-md bg-red-400 hover:opacity-90 text-white text-1xl'>Order Now</button>
