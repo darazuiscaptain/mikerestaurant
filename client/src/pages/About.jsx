2cf116df1f3407e0c0eeec1b81c398564eb0eabb
darazuiscaptain <smallcaptain@proton.me>
2023-11-07 11:25:34 +0300

    header and footer included

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 function About() {
   return (
-    <div className='flex flex-col gap-5 max-w-[500px] mx-auto px-3 font-serif sm:mt-8 md:mt-12 lg:mt-16'>
-      <h1 className='text_gradient_h text-2xl uppercase font-semibold'>Lorem ipsum dolor</h1>
-      <p className=''>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Odio similique voluptatum magnam earum fuga libero tempora sint nobis ipsa voluptates.
-        Culpa error earum laborum accusantium.
-      </p>
+    <div className='flex flex-col'>
+      <Header/>
+      <div className='flex flex-col gap-5 h-[70vh] max-w-[500px] mx-auto px-3 font-serif mt-4 sm:mt-8 md:mt-12 lg:mt-16'>
+        <h1 className='text_gradient_h text-2xl uppercase font-semibold'>Lorem ipsum dolor</h1>
+        <p className=''>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Odio similique voluptatum magnam earum fuga libero tempora sint nobis ipsa voluptates.
+          Culpa error earum laborum accusantium.
+        </p>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Ut, distinctio placeat ea neque qui corrupti sunt rem sed culpa maxime magni, 
-        quae repudiandae blanditiis similique ipsam dolor ipsum exercitationem optio! 
-        Aliquam similique quis voluptates eaque.
-      </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Ut, distinctio placeat ea neque qui corrupti sunt rem sed culpa maxime magni,
+          quae repudiandae blanditiis similique ipsam dolor ipsum exercitationem optio!
+          Aliquam similique quis voluptates eaque.
+        </p>
 
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-        Veritatis sapiente eius dolor temporibus perferendis adipisci incidunt ut perspiciatis 
-        voluptatem, 
-        enim iste repellendus reiciendis repellat rerum distinctio aperiam itaque.
-      </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Veritatis sapiente eius dolor temporibus perferendis adipisci incidunt ut perspiciatis
+          voluptatem,
+          enim iste repellendus reiciendis repellat rerum distinctio aperiam itaque.
+        </p>
+      </div>
+      <Footer/>
     </div>
   )
 }
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,61 +1,67 @@
 import { Card } from '@material-tailwind/react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 function Contact() {
   const emailTo = "smikiyas37@gmail.com"
-  const [ emailFrom, sendEmailFrom ] = useState({
+  const [emailFrom, sendEmailFrom] = useState({
     full_name: "",
     email: "",
     subject: "",
     message: ""
   })
   const handleChange = (e) => {
-    sendEmailFrom({...emailFrom, [e.target.name]: e.target.value})
+    sendEmailFrom({ ...emailFrom, [e.target.name]: e.target.value })
   }
 
   const { full_name, email, subject, message } = emailFrom
   return (
-    <section className='flex flex-col justify-center gap-5 w-full max-w-[500px] mx-auto sm:mt-8 md:mt-12 lg:mt-16'>
-      <Card className='flex flex-col justify-between px-5 sm:px-12 drop-shadow-2xl '>
-        <h1 className='text_gradient_p text-2xl text-center underline-offset-2 pt-2 pt-3'>Contact Us</h1>
-        <form className='flex flex-col w-full h-full gap-3 my-4'>
-          <input 
-            type="text" 
-            name="full_name"
-            value={full_name}
-            onChange={handleChange}
-            placeholder='Full name' 
-            className='hover:outline-none placeholder:text-blue-gray-100 focus:outline-none p-[6px] border-[0.7px] border-gray-400' />
-          <input 
-            type="text" 
-            name="email"
-            value={email}
-            onChange={handleChange}
-            placeholder='Email' 
-            className='hover:outline-none placeholder:text-blue-gray-100 focus:outline-none p-[6px] border-[0.7px] border-gray-400'/>
-          <input 
-            type="text" 
-            name="subject"
-            value={subject}
-            onChange={handleChange}
-            placeholder='Subject' 
-            className='hover:outline-none placeholder:text-blue-gray-100 focus:outline-none p-[6px] border-[0.7px] border-gray-400'/>
-          <textarea 
-            name="message" 
-            value={message}
-            onChange={handleChange}
-            className='max-h-24 hover:outline-none placeholder:text-blue-gray-100 focus:outline-none p-[6px] border-[0.7px] border-gray-400' 
-            cols="10" rows="10" placeholder='Write here..'>
-          </textarea>
-          <Link 
-            to={`mailto:${emailTo}?subject=Regarding ${subject}&body=HeyI'm ${full_name},  ${message}`}
-            className='text-center w-full uppercase p-3 rounded-lg my-3 bg-[#51b75a] text-white opacity-90'>
-            send
-          </Link>
-        </form>
-      </Card>
-    </section>
-  ) 
+    <div className='flex flex-col'>
+      <Header />
+      <section className='flex flex-col justify-center gap-5 w-full max-w-[500px] mx-auto mt-4 sm:mt-8 md:mt-12 lg:mt-16'>
+        <Card className='flex flex-col justify-between px-5 sm:px-12 drop-shadow-2xl '>
+          <h1 className='text_gradient_p text-2xl text-center underline-offset-2 pt-2 pt-3'>Contact Us</h1>
+          <form className='flex flex-col w-full h-full gap-3 my-4'>
+            <input
+              type="text"
+              name="full_name"
+              value={full_name}
+              onChange={handleChange}
+              placeholder='Full name'
+              className='hover:outline-none placeholder:text-blue-gray-100 focus:outline-none p-[6px] border-[0.7px] border-gray-400' />
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              placeholder='Email'
+              className='hover:outline-none placeholder:text-blue-gray-100 focus:outline-none p-[6px] border-[0.7px] border-gray-400' />
+            <input
+              type="text"
+              name="subject"
+              value={subject}
+              onChange={handleChange}
+              placeholder='Subject'
+              className='hover:outline-none placeholder:text-blue-gray-100 focus:outline-none p-[6px] border-[0.7px] border-gray-400' />
+            <textarea
+              name="message"
+              value={message}
+              onChange={handleChange}
+              className='max-h-24 hover:outline-none placeholder:text-blue-gray-100 focus:outline-none p-[6px] border-[0.7px] border-gray-400'
+              cols="10" rows="10" placeholder='Write here..'>
+            </textarea>
+            <Link
+              to={`mailto:${emailTo}?subject=Regarding ${subject}&body=HeyI'm ${full_name},  ${message}`}
+              className='text-center w-full uppercase p-3 rounded-lg my-3 bg-[#51b75a] text-white opacity-90'>
+              send
+            </Link>
+          </form>
+        </Card>
+      </section>
+      <Footer />
+    </div>
+  )
 }
 export default Contact
