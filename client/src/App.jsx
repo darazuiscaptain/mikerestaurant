e0a5b1ab447578a337ab174d34636d4e390679f7
darazuiscaptain <smallcaptain@proton.me>
2023-11-07 17:15:34 +0300

    protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,16 @@ import PageNotFound from "./pages/PageNotFound";
 import Profile from "./pages/Profile";
 import Cart from "./pages/Cart";
 import Order from "./pages/Order";
+import { useSelector } from "react-redux";
+import Dashboard from "./pages/admin/dashboard";
+
 
 
 function App() {
   useEffect(() => {
     Aos.init()
   }, [])
+
 
   return (
     <>
@@ -27,16 +31,46 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="sign_up_in" element={<Sign_up_in />} />
-          <Route path="profile/me" element={<Profile />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="order" element={<Order />} />
+
+          {/* Protected Customers Routes */}
+          <Route path="profile/me" element={<protectedRoute><Profile /></protectedRoute>} />
+          <Route path="cart" element={<protectedRoute><Cart /></protectedRoute>} />
+          <Route path="order" element={<protectedRoute><Order /></protectedRoute>} />
+
+
+          {/* Protected Admin Routes */}
+          <Route path="dashboard" element={<protectedAdminRoute><Dashboard /> </protectedAdminRoute>} />
+
+
+          {/* Routes Not Found  */}
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-        <ToastContainer/>
+        <ToastContainer />
       </BrowserRouter>
 
     </>
   )
+}
+
+export const protectedRoute = ({ children }) => {
+  const { currentUser } = useSelector((state) => state.user)
+
+  if (currentUser?.role === "customer") {
+    return (
+      { children }
+    )
+  }
+  else return null
+}
+
+export const protectedAdminRoute = ({ children }) => {
+  const { currentUser } = useSelector((state) => state.user)
+
+  if (currentUser?.role === "admin") {
+    return (
+      { children }
+    )
+  } else return null
 }
 
 export default App
