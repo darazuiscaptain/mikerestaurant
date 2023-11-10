c6e6f0627c003e40217f414633e67f9cfe9a8ffd
darazuiscaptain <smallcaptain@proton.me>
2023-11-10 23:47:38 +0300

    redux-persist

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,26 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { configureStore, getDefaultMiddleware, combineReducers } from '@reduxjs/toolkit'
 import authReducer from '../redux/authSlice'
+import cartReducer from "../redux/cartSlice"
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducers = combineReducers({
+  auth: authReducer,
+  cart: cartReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false
   })
-})
+});
+
+export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
-import { store } from './app/store'
+import { store, persistor } from './app/store'
 import { Provider } from 'react-redux'
- 
+import { PersistGate } from 'redux-persist/integration/react';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </PersistGate>
   </Provider>
 );
