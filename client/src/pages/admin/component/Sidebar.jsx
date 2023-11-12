2ac73ee5faf4b60e917a2fd905b27c7043b7ce71
darazuiscaptain <smallcaptain@proton.me>
2023-11-13 01:05:20 +0300

    current user info

--- client/src/pages/admin/component/Sidebar.jsx
+++ client/src/pages/admin/component/Sidebar.jsx
@@ -1,21 +1,23 @@
 import { Link } from 'react-router-dom'
 import Logo from '../../../components/Logo'
 import { AiTwotoneHome } from "react-icons/ai"
-import { GrTransaction } from "react-icons/gr"
 import { MdOutlineProductionQuantityLimits, MdRememberMe } from "react-icons/md"
 import { HiDocumentReport } from "react-icons/hi"
 import { FaSalesforce } from "react-icons/fa"
+import { useSelector } from 'react-redux'
 
 
 const Sidebar = () => {
+    const { currentUser } = useSelector((state) => state.auth)
+    const { username, email } = currentUser
     return (
         <div className='flex w-[11rem] h-[100vh] sticky top-0 left-0 rounded-none border-r-2'>
             <div className='flex flex-col mx-auto pt-7 gap-6'>
                 <div className='flex flex-col gap-4'>
                     <Logo />
                     <div className='flex flex-col items-center justify-center'>
-                        <h2 className='text-sm font-normal'>Mikiyas</h2>
-                        <h5 className='text-xs text-gray-500'>smikiyas37@gmail.com</h5>
+                        <h2 className='text-sm font-normal'>{username}</h2>
+                        <h5 className='text-xs text-gray-500'>{email}</h5>
                     </div>
                 </div>
                 <div></div>
