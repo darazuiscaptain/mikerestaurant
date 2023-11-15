c6724ca3470943f1a54159c233f3454cc30e2c50
darazuiscaptain <smallcaptain@proton.me>
2023-11-15 20:13:24 +0300

    typo

--- client/src/pages/delivery/Dashboard.jsx
+++ client/src/pages/delivery/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Sidebar from './component/SideBar'
+import SideBar from './component/SideBar'
 import MainBoard from './component/MainBoard'
 
 const Dashboard = () => {
   return (
     <div id="dashboard" className='flex w-full h-full'>
       <div id="sidebar">
-        <Sidebar/>
+        <SideBar/>
       </div>
       <div id="main" className='flex flex-1 bg-blue-gray-50'>
         <MainBoard/>
