aa6c8b6dbbc6e35854156c3f2bff2df46374f926
darazuiscaptain <smallcaptain@proton.me>
2023-11-15 20:18:59 +0300

    typo

--- client/src/pages/delivery/Dashboard.jsx
+++ client/src/pages/delivery/Dashboard.jsx
@@ -7,10 +7,6 @@ const Dashboard = () => {
     <div id="dashboard" className='flex w-full h-full'>
       <div id="sidebar">
         <SideBar/>
-=======
-      <div id="sideBar">
-        <Sidebar/>
->>>>>>> 79bcade8376d681d6cdda4d5e6a78792d3f8e487
       </div>
       <div id="main" className='flex flex-1 bg-blue-gray-50'>
         <MainBoard/>
