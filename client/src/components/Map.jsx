86306f8e345fa08ab3794c631b32e19d59d25cad
darazuiscaptain <smallcaptain@proton.me>
2023-11-16 18:36:27 +0300

    UI and Functionality modification

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 
 function Map() {
     return (
-        <div>
+        <div >
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252230.25579726885!2d38.61333128434561!3d8.96314036928755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85cef5ab402d%3A0x8467b6b037a24d49!2sAddis%20Ababa!5e0!3m2!1sen!2set!4v1698784084311!5m2!1sen!2set"
-                width="600"
-                height="450"
-                style="border:0;"
                 allowfullscreen=""
                 loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade">
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -10,6 +10,7 @@ import LoadingSpinner from '../components/LoadingSpinner';
 import fetchAPI from '../utils/fetchData/fetchAPI';
 
 import { BASE_URL } from "../baseurl"
+import Map from '../components/Map';
 
 const Cart = () => {
 
@@ -36,6 +37,8 @@ const Cart = () => {
         toast("item removed")
     }
 
+    const [location, setLocation] = useState("")
+
 
     const handleSubmit = async () => {
         const productId = []
@@ -50,23 +53,26 @@ const Cart = () => {
         // console.log(productId, "id")
         const order = {
             customer: currentUser?._id,
+            location,
             items: productId,
-            totalAmount: subtotal+15 || product?.price+15
+            totalAmount: subtotal + 15 || product?.price + 15
         }
-        console.log(order, "order")
 
         if (cart.length < 1 && Object.keys(id).length === 0) {
             toast.error("Invalid Request!")
+        } else if(location === ""){
+            toast("Please provide delivery location!")
         } else {
             try {
                 setrPocessing(true)
-                await axios.post(`${BASE_URL}/orders/create-order`, order)
+                const res = await axios.post(`${BASE_URL}/orders/create-order`, order)
                 if (typeof id !== 'object' || Object.keys(id).length === 0) {
                     dispatch(deleteAllCart())
                 }
                 toast.success("Order has been added! thank you for choosing us.")
                 navigate("/")
                 setrPocessing(false)
+                console.log(res)
             } catch (error) {
                 toast.error(error)
             } finally {
@@ -95,109 +101,114 @@ const Cart = () => {
             }
         }
         fetchSingleProduct()
-        console.log(cart)
-        console.log(id)
+        
     }, [])
 
     return (
         <section id="cart" className="flex flex-col p-2 ">
-            <div className='flex flex-col mt-4 mx-12  max-h-[60vh]'>
-                <h1 className='text-red-700 font-bold text-2xl whitespace-nowrap'>
+            <div className='flex flex-col mt-4 mx-5  max-h-[75vh] overflow-y-scroll'>
+                <h1 className='text-red-400 font-bold text-2xl whitespace-nowrap'>
                     Your Cart is Here
                 </h1>
                 <div className="w-full border-b-2 border-gray-500 my-3" />
-                <div className='grid sm:grid-cols-2 grid-rows-2 gap-0 sm:gap-8'>
-                    {
-                        loading
-                            ? <LoadingSpinner size={50} />
-                            : Object.keys(id).length === 0 ? (
-                                cart && (
-                                    <div className='flex flex-wrap gap-8'>
-                                        {cart.filter(item => item).map((item, index) => (
-                                            item && <div key={index}>
-                                                <div
-                                                    className='flex flex-col gap-2 min-w-[260px] h-24 p-2 border-[1px] bg-blue-50 rounded-r-lg'>
-                                                    <div className='flex flex-wrap'>
-                                                        <div>
-                                                            <img src={item.productImage} alt={item.productName} className='w-24 h-20 rounded-md' />
-                                                        </div>
-                                                        <div className='flex flex-col justify-between px-2 p-1 flex-1'>
-                                                            <div className='flex flex-col gap-1 min-w-full'>
-                                                                <h5 className='text-md truncate font-bold  capitalize'>{item.productName}</h5>
-                                                                <div className='flex items-center justify-between px-1'>
-                                                                    <span className='text-xs '>${item.price}</span>
-                                                                </div>
-                                                                <div className='flex justify-between'>
-                                                                </div>
-                                                                <div className='text-md' onClick={() => removeCart(item)}>
-                                                                    <AiFillDelete className='text-red-400 cursor-pointer' />
+                <h2
+                    className="w-full p-2 uppercase text-xs hover:cursor-pointer my-2 text-red-400 rounded-md whitespace-nowrap px-2 hover:opacity-90"
+                    onClick={() => deleteCart()}>
+                    Clear cart
+                </h2>
+                <div className='flex flex-col md:flex-row gap-8'>
+                    <div className='flex flex-1'>
+                        {
+                            loading
+                                ? <LoadingSpinner size={50} />
+                                : Object.keys(id).length === 0 ? (
+                                    cart && (
+                                        <div className='flex flex-wrap gap-4'>
+                                            {cart.filter(item => item).map((item, index) => (
+                                                item && <div key={index}>
+                                                    <div
+                                                        className='flex flex-col gap-2 min-w-[260px] h-24 p-2 border-[1px] bg-gray-50 rounded-r-lg'>
+                                                        <div className='flex flex-wrap'>
+                                                            <div>
+                                                                <img src={item.productImage} alt={item.productName} className='w-24 h-20 rounded-md' />
+                                                            </div>
+                                                            <div className='flex flex-col justify-between px-2 p-1 flex-1'>
+                                                                <div className='flex flex-col gap-1 min-w-full'>
+                                                                    <h5 className='text-md truncate font-bold  capitalize'>{item.productName}</h5>
+                                                                    <div className='flex items-center justify-between px-1'>
+                                                                        <span className='text-xs '>${item.price}</span>
+                                                                    </div>
+                                                                    <div className='flex justify-between'>
+                                                                    </div>
+                                                                    <div className='text-md' onClick={() => removeCart(item)}>
+                                                                        <AiFillDelete className='text-red-400 cursor-pointer' />
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        ))
-                                        }
-                                        <h2
-                                            className="w-full p-2 uppercase text-xs hover:cursor-pointer my-6 text-red-400 rounded-md whitespace-nowrap px-2 hover:opacity-90"
-                                            onClick={() => deleteCart()}>
-                                            Clear cart
-                                        </h2>
-                                    </div>
-                                )
-                            ) : (
-                                <div>
-                                    {product && (
-                                        <div className="flex gap-2 max-w-[300px] h-24 p-2 border-[1px] bg-blue-50 rounded-r-lg">
-                                            <div>
-                                                <img src={product.productImage} alt={product.productName} className='w-24 h-20 rounded-md' />
-                                            </div>
-                                            <div className='flex flex-col justify-between px-2 p-1 flex-1'>
-                                                <div className='flex flex-col justify-around min-w-full'>
-                                                    <h5 className='text-md truncate font-bold  capitalize'>{product.productName}</h5>
-                                                    <div className='flex items-center justify-between px-1'>
-                                                        <span className='text-xs '>${product?.price}</span>
+                                            ))
+                                            }
+
+                                        </div>
+                                    )
+                                ) : (
+                                    <div>
+                                        {product && (
+                                            <div className="flex gap-2 max-w-[300px] h-24 p-2 border-[1px] bg-blue-50 rounded-r-lg">
+                                                <div>
+                                                    <img src={product.productImage} alt={product.productName} className='w-24 h-20 rounded-md' />
+                                                </div>
+                                                <div className='flex flex-col justify-between px-2 p-1 flex-1'>
+                                                    <div className='flex flex-col justify-around min-w-full'>
+                                                        <h5 className='text-md truncate font-bold  capitalize'>{product.productName}</h5>
+                                                        <div className='flex items-center justify-between px-1'>
+                                                            <span className='text-xs '>${product?.price}</span>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </div>)}
-                                </div>
-                            )
-                    }
-
-
-                    {/* Order summary */}
-                    <div className="flex flex-col justify-between w-[350px] h-fit p-3 mt-3 bg-white shadow-2xl rounded-xl">
-                        <div className="flex flex-col gap-3">
-                            <h2 className="flex justify-center text-red-400 text-xl">Order Summary</h2>
-                            <div className="flex flex-col p-3">
-                                <div className="flex justify-between">
-                                    <h2 className="text-sm">Subtotal</h2>
-                                    <span>${Object.keys(id).length === 0 ? subtotal : product?.price} </span>
-                                </div>
-                                <div className="flex justify-between">
-                                    <h2 className="text-sm">Shipping cost</h2>
-                                    <span>$15</span>
-                                </div>
-                                <div className="flex justify-between">
-                                    <h2 className="text-sm">Total</h2>
-                                    <span> ${Object.keys(id).length === 0 ? subtotal + 15 : product?.price + 15}  </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex gap-3 p-3 w-[100%]">
-                            <button
-                                onClick={() => handleSubmit()}
-                                disabled={processing}
-                                className="p-2 flex justify-center w-full rounded-md text-white text-sm bg-teal-500">
-                                {processing ? <LoadingSpinner size={20} color={"#fff"}/> : "Proceed to checkout" }
-                            </button>
-                        </div>
+                                            </div>)}
+                                    </div>
+                                )
+                        }
                     </div>
 
+                    <div className='flex justify-start flex-col flex-1 gap-4'>
+                        <div className='flex flex-col gap-2  max-w-[350px]'>
+                            <h2 className='text-md font-light'>Your specific Location</h2>
+                            <input type="text" name='location' onChange={(e) => setLocation(e.target.value)} className='bg-gray-200 p-2 border-[1px]  border-red-100' />
+                        </div>
+                         {/* Order summary */}
+                        <div className="flex flex-col justify-end max-w-[350px] md:w-full h-fit p-3 mt-3 bg-white shadow-2xl rounded-xl">
+                            <div className="flex flex-col gap-3">
+                                <h2 className="flex justify-center text-red-400 text-xl">Order Summary</h2>
+                                <div className="flex flex-col p-3">
+                                    <div className="flex justify-between">
+                                        <h2 className="text-sm">Subtotal</h2>
+                                        <span>${Object.keys(id).length === 0 ? subtotal : product?.price} </span>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <h2 className="text-sm">Shipping cost</h2>
+                                        <span>$15</span>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <h2 className="text-sm">Total</h2>
+                                        <span> ${Object.keys(id).length === 0 ? subtotal + 15 : product?.price + 15}  </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex gap-3 p-3 w-[100%]">
+                                <button
+                                    onClick={() => handleSubmit()}
+                                    disabled={processing}
+                                    className="p-2 flex justify-center w-full rounded-md text-white text-sm bg-teal-500">
+                                    {processing ? <LoadingSpinner size={20} color={"#fff"} /> : "Proceed to checkout"}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </section>
 
