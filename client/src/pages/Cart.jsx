8fc0327d137c9261ee40fb3c1372445b3f8c3bac
darazuiscaptain <smallcaptain@proton.me>
2023-11-19 21:48:05 +0300

    apply quantity

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,16 +1,16 @@
 
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteAllCart, removeFromCart } from '../redux/cartSlice';
-import { AiFillDelete } from 'react-icons/ai';
 import { toast } from 'react-toastify';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import LoadingSpinner from '../components/LoadingSpinner';
 import fetchAPI from '../utils/fetchData/fetchAPI';
 
+import { deleteAllCart, decreaseCart, increaseCart, cartTotal } from '../redux/cartSlice';
+// addToCart, 
 import { BASE_URL } from "../baseurl"
-import Map from '../components/Map';
+
 
 const Cart = () => {
 
@@ -20,22 +20,26 @@ const Cart = () => {
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const { cart } = useSelector(state => state.cart);
+    const { cartItems, cartTotalAmount } = useSelector(state => state.cart);
 
     const [product, setProduct] = useState({})
 
     const { currentUser } = useSelector((state) => state.auth)
 
-    const subtotal = cart.reduce(
-        (total, item) => total + item?.price * item?.quantity, 0)
+    const handleDecrease = (item) => {
+        dispatch(decreaseCart(item))
+    }
+    const handleIncrease = (item) => {
+        dispatch(increaseCart(item))
+    }
+    // let total = dispatch(cartTotal())
+    // console.log(total)
 
     const deleteCart = () => {
         dispatch(deleteAllCart())
     }
-    const removeCart = (item) => {
-        dispatch(removeFromCart(item))
-        toast("item removed")
-    }
+
+    // const subtotal = carCtItems.reduce((total, item) => total + item?.price * item?.quantity, 0)
 
     const [location, setLocation] = useState("")
 
@@ -46,7 +50,7 @@ const Cart = () => {
             const _id = (id.id).toString()
             productId.push(_id)
         } else {
-            cart.forEach(element => {
+            cartItems.forEach(element => {
                 productId.push(element._id)
             })
         }
@@ -55,12 +59,12 @@ const Cart = () => {
             customer: currentUser?._id,
             location,
             items: productId,
-            totalAmount: subtotal + 15 || product?.price + 15
+            totalAmount: product?.price + 15 || cartTotalAmount + 15 
         }
 
-        if (cart.length < 1 && Object.keys(id).length === 0) {
+        if (cartItems.length < 1 && Object.keys(id).length === 0) {
             toast.error("Invalid Request!")
-        } else if(location === ""){
+        } else if (location === "") {
             toast("Please provide delivery location!")
         } else {
             try {
@@ -101,8 +105,12 @@ const Cart = () => {
             }
         }
         fetchSingleProduct()
-        
+
     }, [])
+
+
+    dispatch(cartTotal())
+  
 
     return (
         <section id="cart" className="flex flex-col p-2 ">
@@ -122,9 +130,9 @@ const Cart = () => {
                             loading
                                 ? <LoadingSpinner size={50} />
                                 : Object.keys(id).length === 0 ? (
-                                    cart && (
+                                    cartItems && (
                                         <div className='flex flex-wrap gap-4'>
-                                            {cart.filter(item => item).map((item, index) => (
+                                            {cartItems.filter(item => item).map((item, index) => (
                                                 item && <div key={index}>
                                                     <div
                                                         className='flex flex-col gap-2 min-w-[260px] h-24 p-2 border-[1px] bg-gray-50 rounded-r-lg'>
@@ -133,15 +141,20 @@ const Cart = () => {
                                                                 <img src={item.productImage} alt={item.productName} className='w-24 h-20 rounded-md' />
                                                             </div>
                                                             <div className='flex flex-col justify-between px-2 p-1 flex-1'>
-                                                                <div className='flex flex-col gap-1 min-w-full'>
-                                                                    <h5 className='text-md truncate font-bold  capitalize'>{item.productName}</h5>
-                                                                    <div className='flex items-center justify-between px-1'>
-                                                                        <span className='text-xs '>${item.price}</span>
-                                                                    </div>
-                                                                    <div className='flex justify-between'>
-                                                                    </div>
-                                                                    <div className='text-md' onClick={() => removeCart(item)}>
-                                                                        <AiFillDelete className='text-red-400 cursor-pointer' />
+                                                                <div className='flex justify-between min-w-full'>
+                                                                    <div className='flex flex-col gap-2'>
+                                                                        <h5 className='text-md truncate font-bold  capitalize'>{item.productName}</h5>
+                                                                        <div className='flex items-center justify-between px-1'>
+                                                                            <span className='text-xs '>${item.price}</span>
+                                                                        </div>
+
+                                                                        <div className='flex gap-2 items-center'>
+                                                                            <span onClick={() => handleDecrease(item)}
+                                                                                className='cursor-default text-red-600 text-2xl font-extrabold'>-</span>
+                                                                            <span className=''>{item.cartQuantity}</span>
+                                                                            <span onClick={() => handleIncrease(item)}
+                                                                                className=' cursor-default text-teal-600 text-2xl font-extrabold'>+</span>
+                                                                        </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -179,22 +192,22 @@ const Cart = () => {
                             <h2 className='text-md font-light'>Your specific Location</h2>
                             <input type="text" name='location' onChange={(e) => setLocation(e.target.value)} className='bg-gray-200 p-2 border-[1px]  border-red-100' />
                         </div>
-                         {/* Order summary */}
+                        {/* Order summary */}
                         <div className="flex flex-col justify-end max-w-[350px] md:w-full h-fit p-3 mt-3 bg-white shadow-2xl rounded-xl">
                             <div className="flex flex-col gap-3">
                                 <h2 className="flex justify-center text-red-400 text-xl">Order Summary</h2>
                                 <div className="flex flex-col p-3">
                                     <div className="flex justify-between">
                                         <h2 className="text-sm">Subtotal</h2>
-                                        <span>${Object.keys(id).length === 0 ? subtotal : product?.price} </span>
+                                        <span>${Object.keys(id).length === 0 ? cartTotalAmount : product?.price} </span>
                                     </div>
                                     <div className="flex justify-between">
                                         <h2 className="text-sm">Shipping cost</h2>
                                         <span>$15</span>
                                     </div>
                                     <div className="flex justify-between">
-                                        <h2 className="text-sm">Total</h2>
-                                        <span> ${Object.keys(id).length === 0 ? subtotal + 15 : product?.price + 15}  </span>
+                                        <h2 className="text-sm">Total </h2>
+                                        <span> ${Object.keys(id).length === 0 ? cartTotalAmount + 15 : product?.price + 15}  </span>
                                     </div>
                                 </div>
                             </div>
--- client/src/redux/cartSlice.js
+++ client/src/redux/cartSlice.js
@@ -1,46 +1,68 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    cart: [],
-    total: 0,
-    subtotal: 0,
+    cartItems:[],
+    cartQuantity: 0,
+    cartTotalAmount:100,
+    quantity:0
 }
 
 export const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        addToCart: (state, action) => {
-            state.cart.push(action.payload);
-            state.total += action.payload.price * action.payload.quantity;
-            state.subtotal += action.payload.price;
-        },
-        getCart: (state) => {
-            return state;
-        },
-        removeFromCart: (state, action) => {
-            const index = state.cart.findIndex(item => item._id === action.payload._id);
-            if (index >= 0) {
-                state.total -= state.cart[index].price * state.cart[index].quantity;
-                state.subtotal -= state.cart[index].price;
-                state.cart.splice(index, 1);
+        addToCart:(state,action)=>{
+            const itemIndex= state.cartItems.findIndex((item)=>item._id === action.payload._id)
+            if(itemIndex >=0){
+                state.cartItems[itemIndex].cartQuantity +=1;
+            }else{
+                const tempProduct ={...action.payload,cartQuantity :1}
+                state.cartItems.push(tempProduct)
             }
         },
-        deleteAllCart: (state) => {
-            state.cart = []
-        }, 
-        
-        setCart: (state, action) => {
-            state.cart = []
-            state.cart.push(action.payload)
-        }
 
+        removeFromCart:(state,action)=>{
+            const removedItem = state.cartItems.filter((cartItem)=>cartItem._id !== action.payload._id);
+            state.cartItems = removedItem
+        },
+
+        decreaseCart:(state,action)=>{
+            const itemIndex = state.cartItems.findIndex((cartItem)=>cartItem._id === action.payload._id);
+            if(state.cartItems[itemIndex].cartQuantity > 1){
+                state.cartItems[itemIndex].cartQuantity -= 1
+                
+            } else if(state.cartItems[itemIndex].cartQuantity === 1){
+                const removedItem = state.cartItems.filter((cartItem)=>cartItem._id !== action.payload._id);
+                state.cartItems = removedItem
+            }
+        },
+
+        increaseCart:(state,action)=>{
+            const itemIndex = state.cartItems.findIndex((cartItem)=>cartItem._id === action.payload._id)
+            state.cartItems[itemIndex].cartQuantity +=1
+        },
+
+        deleteAllCart:(state)=>{
+            state.cartItems = [];
+        },
+
+        cartTotal:(state)=>{
+            let {total,quantity} =  state.cartItems.reduce((cartTotal,cartItem)=>{
+                const{price,cartQuantity}=cartItem;
+                const itemTotal = price * cartQuantity;
+                cartTotal.total+=itemTotal;
+                cartTotal.quantity+=cartQuantity;
+                return cartTotal
+            },  {total:0,quantity:0})
+            state.cartQuantity =quantity ;
+            state.cartTotalAmount =total;
+        },
     },
 })
 
 // Action creators are generated for each case reducer function
 export const {
-    addToCart, getCart, removeFromCart, deleteAllCart, setCart
+    addToCart, decreaseCart, increaseCart, removeFromCart, deleteAllCart, cartTotal
 } = cartSlice.actions
 
 export default cartSlice.reducer
